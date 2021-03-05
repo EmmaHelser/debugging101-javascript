5276c485b6bf97f873ef679ac6430eb58defb14f
EmmaHelser <77553206+EmmaHelser@users.noreply.github.com>
2021-03-04 18:41:59 -0800

    Update FE_2.js

--- Exercises/1. Foundation/FE_2.js
+++ Exercises/1. Foundation/FE_2.js
@@ -1,3 +1,2 @@
 /* Identify the issue in the following code.*/
-const javaScriptWorkshops;
-javaScriptWorkshops = ['JavaScript 101', 'JavaScript 201', 'JavaScript 301', 'JavaScript 401', 'Thinking Like a Software Engineer Part 1'];
+const javaScriptWorkshops = ['JavaScript 101', 'JavaScript 201', 'JavaScript 301', 'JavaScript 401', 'Thinking Like a Software Engineer Part 1'];
